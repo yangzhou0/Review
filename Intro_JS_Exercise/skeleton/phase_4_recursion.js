5250ec91641f9c438d0d5b1808e56555ddd1bd48
Yang Zhou <yzhou3991@gmail.com>
2018-04-09 10:37:00 -0400

    exponential function using recurssion

--- Intro_JS_Exercise/skeleton/phase_4_recursion.js
+++ Intro_JS_Exercise/skeleton/phase_4_recursion.js
@@ -1,3 +1,4 @@
+'use strict'
 var range = (start,end) => {
   var result = [start];
   if (start === end) {return [end];}
@@ -6,4 +7,26 @@ var range = (start,end) => {
 
 }
 
-sumRec(arr)
+var sumRec = (arr) => {
+  if (arr.length === 0){return 0;}
+  var sum = arr.pop();
+  sum += sumRec(arr);
+  return sum;
+}
+
+
+function exponent(base, exp) {
+  if (exp === 0) {return 1;}
+  let result = base * exponent(base,exp-1);
+  return result;
+}
+
+function exponent2(base,exp){
+  if (exp === 0){return 1;}
+  else if (exp%2 === 0){
+    return Math.pow(exponent2(base,exp/2), 2);
+  }
+  else {
+    return Math.pow(exponent2(base, (exp - 1) / 2),2) * base;
+  }
+}
