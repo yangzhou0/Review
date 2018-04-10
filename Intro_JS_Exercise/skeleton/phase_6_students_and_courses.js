afec76df8906b3c65e11898638a07091f14d748a
Yang Zhou <yzhou3991@gmail.com>
2018-04-10 19:48:12 -0400

    add in a test case

--- Intro_JS_Exercise/skeleton/phase_6_students_and_courses.js
+++ Intro_JS_Exercise/skeleton/phase_6_students_and_courses.js
@@ -30,3 +30,15 @@ Student.prototype.courseLoad = function(){
     return accu;
   },{});
 };
+
+//comment in below to test
+// let student1 = new Student("Nigel", "Leffler");
+// let course1 = new Course("101", "CS", 3, ["mon", "wed", "fri"], 1);
+// let course2 = new Course("201", "CS", 3, ["wed"], 1);
+// let course3 = new Course("301", "ENG", 3, ["tue"], 1);
+// let course4 = new Course("401", "BIO", 3, ["mon", "wed", "fri"], 2);
+// console.log(student1.name());
+// student1.enroll(course1);
+// student1.enroll(course3);
+// student1.enroll(course2);
+// console.log(student1.courseLoad());
